fc7a662f7c616e75fc0ef1f9eadbc6280f7a9d56
Andre Luiz <andreluizrc27@gmail.com>
2024-04-08 22:31:05 -0300

    feat: inicialização da função de preenchimento dos personagens

--- Questao1.java
+++ Questao1.java
@@ -1,4 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Scanner;
 
 class Lista {
 
@@ -16,9 +23,9 @@ class Personagem {
     private String hogwatsStudent;
     private String actorName;
     private boolean alive;
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     private int yearOfBith;
-    private  String eyeColour;
+    private String eyeColour;
     private String gender;
     private String hairColor;
     private boolean wizard;
@@ -27,7 +34,7 @@ class Personagem {
 
     public Personagem(String id, String name, Lista alternate_names, String house, String ancestry, String species,
             String patronus, boolean hogwartsStaff, String hogwatsStudent, String actorName, boolean alive,
-            Date dateOfBirth, int yearOfBith, String eyeColour, String gender, String hairColor, boolean wizard) {
+            LocalDate dateOfBirth, int yearOfBith, String eyeColour, String gender, String hairColor, boolean wizard) {
         this.id = id;
         this.name = name;
         this.alternate_names = alternate_names;
@@ -59,7 +66,7 @@ class Personagem {
         this.hogwatsStudent = "";
         this.actorName = "";
         this.alive = false;
-        this.dateOfBirth = "00-00-0000";
+        this.dateOfBirth = Date.from(null);
         this.yearOfBith = 0;
         this.eyeColour = "";
         this.gender = "";
@@ -204,42 +211,58 @@ class Personagem {
     }
 
     public void imprimir(){
-        System.out.println(id);
-        System.out.println(name);
-        System.out.println(alternate_names);
-        System.out.println(house);
-        System.out.println(ancestry);
-        System.out.println(species);
-        System.out.println(patronus);
-        System.out.println(hogwartsStaff);
-        System.out.println(hogwatsStudent);
-        System.out.println(actorName);
-        System.out.println(alive);
-        System.out.println(dateOfBirth);
-        System.out.println(yearOfBith);
-        System.out.println(eyeColour);
-        System.out.println(gender);
-        System.out.println(hairColor);
-        System.out.println(wizard);
+        System.out.println("[" + id +"##" + name +"##"+ alternate_names + "##" + house + "##" + ancestry + "##" + species + "##" + patronus + "##" + hogwartsStaff + 
+        "##" + hogwatsStudent + "##" + actorName + "##" + alive + "##" + dateOfBirth + "##" + yearOfBith + "##" + eyeColour + "##" + gender + "##" + hairColor + "##" + wizard + "]");
     }
 
     public Personagem clonar(){
         return this;
     }
 
-    public void ler(String dados){
-        String[] campos = new String[17];
+    public static String[] ler(String dados){
+        String[] atributos = new String[17];
         int tam = dados.length();
         int initialIndex = 0;
+        int posicao_vetor = 0;
         for(int i = 0; i < tam; i++){
             if(dados.charAt(i) == ';'){
-                campos[]
+                atributos[posicao_vetor] = dados.substring(initialIndex, i);
+                initialIndex = i+1;
+                posicao_vetor++;
             }
         }
+
+        return atributos;
     }
 
 }
 
 public class Questao1 {
+
+    public static void main(String[] args) {
+        Personagem[] personagens = new Personagem[405];
+        String line;
+        String[] atributos;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        try {
+            Scanner sc = new Scanner(new File("characters.csv"));
+            int i = 0;
+            while(sc.hasNextLine()){
+                line = sc.nextLine();
+                atributos = Personagem.ler(line);
+                Lista atributo3;
+                personagens[i] = new Personagem(atributos[0],atributos[1],atributo3, atributos[3], atributos[4], atributos[5], atributos[6], Boolean.parseBoolean(atributos[7]), atributos[8], atributos[9], 
+                Boolean.parseBoolean(atributos[10]),LocalDate.parse(atributos[11], formatter), Integer.parseInt(atributos[12]),atributos[13],atributos[14],atributos[15], Boolean.parseBoolean(atributos[16]));
+            }
+
+            sc.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+
+    }
 
 }
